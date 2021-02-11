03152ac789041cff89dcdcf5e0b3a9db5235b03c
ris_vb@126.com <ris_vb@126.com>
2021-02-11 19:05:22 +0800

    first commit

---
+++ GIFCombiner/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace GIFCombiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists("combined")) Directory.CreateDirectory("combined");
+            foreach(string file in args)
+            {
+                Console.WriteLine("Processing: " + file);
+                Bitmap bitmap = (Bitmap)Bitmap.FromFile(file);
+                FrameDimension fd = new FrameDimension(bitmap.FrameDimensionsList[0]);
+                Bitmap convert = new Bitmap(bitmap.Width * bitmap.GetFrameCount(fd), bitmap.Height);
+                Graphics g = Graphics.FromImage((Image)convert);
+                for(int i = 0;i < bitmap.GetFrameCount(fd); i++)
+                {
+                    bitmap.SelectActiveFrame(fd, i);
+                    g.DrawImage(bitmap, new Point(i * bitmap.Width, 0));
+                }
+                string filename = file.Split('\\')[file.Split('\\').Length - 1].Split('.')[0];
+                convert.Save("combined\\" + filename + ".png");
+                Console.WriteLine("Succeed: " + filename + ".png");
+                bitmap.Dispose();
+                g.Dispose();
+                convert.Dispose();
+            }
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Please drag file(s) upon this .exe file to continue.");
+                Console.ReadLine();
+            }
+            else {
+                Console.WriteLine("Finished all tasks, see the result in \\combined\\");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# GIFCombiner
+
+合并GIF帧图像以用于游戏开发。
+
+使用方法：
+
+* 将欲合并的GIF拖放到.exe文件上，即可生成便于游戏制作的png文件
+
+实例：
+
+> 欲转换图片：
+>
+> ![转换前](eg1.gif)
+>
+> 
+>
+> 转换结果：
+>
+> ![转换后](eg2.png)
+
+# GIFCombiner
+
+Combine each frame of .gif file to a .png file which is used to develop games.
+
+Steps：
+
+* Drag .gif file(s) upon the .exe file
+
+Example：
+
+> A .gif file：
+>
+> ![转换前](eg1.gif)
+>
+> 
+>
+> The result：
+>
+> ![转换后](eg2.png)
